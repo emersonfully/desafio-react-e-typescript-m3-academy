b21b57b49beeb896ada412d5aad7ba059f720fdb
Emerson Fully <63175980+emersonfully@users.noreply.github.com>
2023-01-18 22:20:37 -0300

    feature(header): Cria o header para mobile

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./Header.module.scss";
 import logoM3 from "../../assets/imgs/logoM3Academy.png";
+import lupa from "../../assets/imgs/lupa.png";
 import cart from "../../assets/imgs/cart.png";
-import { HeaderNav } from "./HeaderNav";
+import menu from "../../assets/svgs/menu-navbar.svg";
+import close from "../../assets/imgs/x-button.png";
+import { HeaderNav } from "./HeaderBotton";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <header>
       <div className={style.header__container}>
@@ -21,22 +25,48 @@ const Header = () => {
             type="text"
             placeholder="Buscar..."
           />
+          <button>
+            <img src={lupa} alt="" />
+          </button>
         </div>
-        <div className={style.container__cart__wrapper}>
-          <a href="/">Entrar</a>
+        <div className={style.menu__headerTop}>
+          <a className={style.headerTop__login} href="/">
+            Entrar
+          </a>
           <a href="/">
             <img
-              className={style.cart__wrapper__icon}
+              className={style.headerTop__cart}
               src={cart}
               alt="Carrinho de compras"
             />
           </a>
         </div>
+        <button
+          className={style.button__navbar}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <img src={menu} alt="" />
+        </button>
+        {isOpen && (
+          <div className={style.mobileNavBar__container}>
+            <div className={style.mobileNavBar__loginWrapper}>
+              <a href="/">Entrar</a>
+              <button onClick={() => setIsOpen(!isOpen)}>
+                {" "}
+                <img src={close} alt="" />{" "}
+              </button>
+            </div>
+            <div className={style.mobileNavBar__items}>
+              <a href="/">Contatos</a>
+              <a href="/">Saiba Mais</a>
+              <a href="/">Institucionais</a>
+            </div>
+          </div>
+        )}
       </div>
       <hr />
       <HeaderNav />
     </header>
   );
 };
-
 export { Header };
